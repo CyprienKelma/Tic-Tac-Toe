91e61ce7872c371b30e5d2329eff94f41fc0404e
Cyprien KELMA <cyprien.kelma@student.junia.com>
2023-07-03 11:48:50 +0200

    Add title + buttons

--- TicTacToe.java
+++ TicTacToe.java
@@ -7,25 +7,51 @@ public class TicTacToe implements ActionListener{
   
   Random random = new Random();
   JFrame frame = new JFrame();
-  JPanel title_Panel = new JPanel();
-  JPanel button_panel = new JPanel();
-  JLabel textfield = new JLabel();
+  JPanel titlePanel = new JPanel();
+  JPanel buttonPanel = new JPanel();
+  JLabel textField = new JLabel();
   JButton[] buttons = new JButton[9];
-  boolean player1_turn;
+  boolean player1Turn;
 
   TicTacToe(){
+
+    /* Window */
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(800, 800);
     frame.getContentPane().setBackground(new Color(50, 50, 50));
     frame.setLayout(new BorderLayout());
     frame.setVisible(true);
 
-    textfield.setBackground(new Color(30, 30 , 30));
-    textfield.setForeground(new Color(25, 0, 255));
-    textfield.setFont(new Font("Verdana", Font.BOLD, 75);
-    textfield.setHorizontalAlignment(JLabel.CENTER);
-    textfield.setText("Tic-Tac-Toe");
-    textfield.setOpaque(true);
+    /* Title part */
+    textField.setBackground(new Color(30, 30 , 30));
+    textField.setForeground(new Color(25, 120, 255));
+    textField.setFont(new Font("Verdana", Font.BOLD, 75));
+    textField.setHorizontalAlignment(JLabel.CENTER);
+    textField.setText("Tic-Tac-Toe");
+    textField.setOpaque(true);
+
+    /* First case : Title */
+    titlePanel.setLayout(new BorderLayout());
+    titlePanel.setBounds(0, 0, 800, 100);
+
+    /* Second case : The game */
+    buttonPanel.setLayout(new GridLayout(3, 3));
+    buttonPanel.setBackground(new Color(150, 150, 150));
+    
+    for(int i = 0; i < 9; i++){
+      buttons[i] = new JButton();
+      buttonPanel.add(buttons[i]);
+      buttons[i].setFont(new Font("Verdana", Font.BOLD, 120));
+      buttons[i].setFocusable(false);
+      buttons[i].addActionListener(this);
+    }
+
+    /* Imbrication */
+    titlePanel.add(textField);
+    frame.add(titlePanel, BorderLayout.NORTH);
+
+    frame.add(buttonPanel);
+
   }
 
   @Override
@@ -42,11 +68,11 @@ public class TicTacToe implements ActionListener{
 
   }
 
-  public void xWins(int , int b, int c){
+  public void xWins(int a, int b, int c){
 
   }
 
-  public void oWins(int , int b, int c){
+  public void oWins(int a, int b, int c){
     
   }
 }
